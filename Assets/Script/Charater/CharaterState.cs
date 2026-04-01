a504b88c6db45957b408fe8e96b7467c2f4c6031
timmy77tw <107679354+timmy77tw@users.noreply.github.com>
2026-04-01 20:49:41 +0800

    20260401
    把昨天進度的一些測試用代碼刪除

--- Assets/Script/Charater/CharaterState.cs
+++ Assets/Script/Charater/CharaterState.cs
@@ -18,19 +18,17 @@ public class CharaterState : MonoBehaviour, CharaterComponent
     {
         if (controll.x == 0 && controll.y == 0) { return; }
         Vector2 movement = controll.normalized * (buffList.GetSpeed(speed) * Time.deltaTime);
-        // Debug.Log($"Time:{Time.time} | movement:{movement} | normalized:{controll.normalized} |controll:{controll} | speed:{buffList.GetSpeed(speed)} | deltaTime:{Time.deltaTime}");
         TranslatePosition(movement);
     }
     public void TranslatePosition(Vector2 vector2)
     {
-        //charaterTransform.transform.position = charaterTransform.transform.position + new Vector3(vector2.x, vector2.y, 0);
         Vector2 direction = vector2.normalized;
         RaycastHit2D hit = Physics2D.Raycast(charaterTransform.position, direction, vector2.magnitude+0.5f, LayerMask.GetMask("Build"));
-        Debug.DrawRay(charaterTransform.position, vector2, Color.red, 1);
+        Debug.DrawRay(charaterTransform.position, -vector2, Color.red, 1);//軌跡
         Debug.Log($"pos:{charaterTransform.position} | vec:{vector2}");
 
-
-        if (hit.collider != null)//未來碰撞架構搭建完後再更改判定
+        //未來碰撞架構搭建完後再更改判定
+        if (hit.collider != null)
         {
             charaterTransform.position = hit.point - direction * 0.5f;
         }
@@ -54,20 +52,6 @@ public class CharaterState : MonoBehaviour, CharaterComponent
     }
     void Update()
     {
-        
-        // float angle = charaterTransform.transform.rotation.eulerAngles.z;
-        // Vector2 movement = (new Vector2((float)Math.Cos(angle/180*Math.PI), (float)Math.Sin(angle/180*Math.PI))).normalized;
-        // Debug.DrawRay(charaterTransform.position, movement, Color.black);
-        // RaycastHit2D hit = Physics2D.Raycast(charaterTransform.position, movement, 1, LayerMask.GetMask("Build"));
-        // if (hit.collider != null)
-        // {
-        //     Debug.Log($"touch : {hit.collider}");
-        //     // charaterTransform.position = hit.point - direction * 0.1f;
-        // }
-        // else
-        // {
-        //     Debug.Log($"untouch");
 
-        // }
     }
 }
